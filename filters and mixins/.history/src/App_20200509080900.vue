12a3a344042a9e6e62fb60838437ce7b8d1ae0cf
Azaria Gebremichael <azariagebremichael10@gmail.com>
2020-05-09 12:07:48 +0300

    practice commit

---
+++ filters and mixins/.history/src/App_20200509080900.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <p>{{text}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+xt
+<script>
+    export default {
+        data: function(){
+            return {
+                text:'Hello world'
+            }
+        },
+        filters: {
+            
+        }
+    }
+</script>
+
+<style>
+
+</style>
